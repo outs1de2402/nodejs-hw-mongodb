3015be697e2bf075d6bd3fb631629d97b08adf4e
outs1de <maksym.dobriden@gmail.com>
2025-08-13 17:39:16 +0300

    !

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -1,18 +1,11 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const swaggerDocument = YAML.load(
-  path.join(__dirname, '../../docs/openapi.yaml'),
-);
+import fs from 'fs';
 
 const router = express.Router();
 
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerFile = JSON.parse(fs.readFileSync('./docs/swagger.json', 'utf8'));
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -31,12 +31,10 @@ export function setupServer() {
   // 🔧 Роути підключаються після всіх парсерів
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
-
+  app.use(docsRouter);
   app.use(notFoundHandler);
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => logger.info(`Server is running on port ${PORT}`));
-
-  app.use(docsRouter);
 }
